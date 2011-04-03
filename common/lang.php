9c75539a3cc63387aac6d66a2ee2c20900c3ae4c
Viacheslav Oliyanchuk <miripiruni@gmail.com>
2011-04-03 20:54:15 +0400

    lang selector

--- common/lang.php
+++ common/lang.php
@@ -1,5 +1,27 @@
 <?
-$lang = 'en';
+
 $lang = 'ru';
+
+if(isset($_COOKIE['lang']) AND !isset($_GET['lang'])){
+	if($_COOKIE['lang'] == 'en'){
+        $lang = 'en';
+    }
+    else if($_COOKIE['lang'] == 'ru'){
+        $lang = 'ru';
+    }
+}
+else{
+    if(isset($_GET['lang']) AND ($_GET['lang']=='ru' or $_GET['lang']=='en')){
+        $lang = $_GET['lang'];
+
+        setcookie('lang', $lang, time()+60*60*24*30*12*3, '/', $_SERVER['HTTP_HOST'], false, true);
+
+        if (!headers_sent()) header('Location: '.str_replace('index.php','',$_SERVER['PHP_SELF']));
+        echo "<script>document.location.href='".str_replace('index.php','',$_SERVER['PHP_SELF'])."';</script>\n";
+        echo "<noscript><a href='".str_replace('index.php','',$_SERVER['PHP_SELF'])."'>click to view the page</a></noscript>\n";
+        exit();
+    }
+}
+
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/static/lang/'.$lang.'.php';
