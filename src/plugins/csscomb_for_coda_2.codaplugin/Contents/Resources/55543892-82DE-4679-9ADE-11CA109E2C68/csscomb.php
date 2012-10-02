b3316f7553b5a833c13204c679475abef3343443
miripiruni <miripiruni@gmail.com>
2012-10-02 19:39:41 +0400

    Fix issue #108: Undefined index: TM_BUNDLE_SUPPORT in Coda 2 plugin

--- src/plugins/csscomb_for_coda_2.codaplugin/Contents/Resources/55543892-82DE-4679-9ADE-11CA109E2C68/csscomb.php
+++ src/plugins/csscomb_for_coda_2.codaplugin/Contents/Resources/55543892-82DE-4679-9ADE-11CA109E2C68/csscomb.php
@@ -6,7 +6,7 @@
  * @author: Vyacheslav Oliyanchuk (miripiruni)
  * @web: http://csscomb.com/
  */
-require_once($_ENV['TM_BUNDLE_SUPPORT'].'/csscomb.php');
+require_once(getenv('TM_BUNDLE_SUPPORT').'/csscomb.php');
 $input = file_get_contents('php://stdin');
 $c = new csscomb();
 echo $c->csscomb($input);?>
